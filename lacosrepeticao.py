90d95c5b8b418fa004eab91d8716a8a4cce091e9
laricris1702 <143663140+laricris1702@users.noreply.github.com>
2023-10-05 21:41:33 -0300

    Add files via upload

---
+++ lacosrepeticao.py
@@ -0,0 +1,37 @@
+#para laco de repeticao while 
+
+#x=0
+#while x < 5 :
+ ##   nome=input ('Qual e seu nome:')
+   # x=x+1
+
+#lista_compras=[]
+#quantidade=0
+#i=0
+#while quantidade < 5:
+   # item_lista=input('Qual item voce quer adicionar na lista:')
+    #lista_compras.append(item_lista)
+    #quantidade=quantidade+1
+#print("A minha lista de compras final:", lista_compras)
+
+
+#faturamento = [100,250,220,500,2000]
+#total=0
+
+#for i in faturamento:
+ #   total = total+i
+
+#print(total)
+
+
+#num = [0,10]
+#for i in range (0,10):
+  #este looping mostrara os numeros ordenados de 0 ate 9)
+
+ # print(i)
+
+lista_num=[]
+num = float(input("Digite um numero:"))
+print('Os valores digitados foram:')
+
+
